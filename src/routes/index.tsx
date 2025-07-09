0816ff04aadebdf56f2e3f7be8da952832aa77be
Mateo Graber <grabermn3@gmail.com>
2025-07-08 21:53:02 -0400

    Set up Open API Key & Fixed vite.config.ts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router';
+import OpenAI from 'openai';
 
 export const Route = createFileRoute('/')({
   component: App,
 });
 
 function App() {
+  const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY, dangerouslyAllowBrowser: true });
+
   return (
     <div style={{ textAlign: 'center', backgroundColor: 'lightgrey', margin: 'auto' }}>
       
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,31 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+/// <reference types="vitest/config" />
+import { defineConfig, loadEnv } from 'vite';
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { resolve } from 'node:path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    viteReact(),
-    tailwindcss(),
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.OPENAI_API_KEY': JSON.stringify(env.OPENAI_API_KEY)
     },
-  },
+    plugins: [
+      TanStackRouterVite({ autoCodeSplitting: true }),
+      viteReact(),
+      tailwindcss(),
+    ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+    },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+    },
+  }
 });
